289d9e9d5aba4e3024458628ca8098390c7553fd
Pawel <jarrodek@gmail.com>
2019-11-16 14:51:28 -0800

    chore: finishing mobile site

--- components/api-console-io/src/ApiConsoleIo.js
+++ components/api-console-io/src/ApiConsoleIo.js
@@ -9,9 +9,9 @@ export class ApiConsoleIo extends LitElement {
   }
 
   render() {
-    return html `
+    return html`
       <header>
-        <h1>API console</h1>
+        <h1>API Console</h1>
         <p class="subtitle">API design for humans</p>
 
         <section class="main-action-buttons">
@@ -26,54 +26,78 @@ export class ApiConsoleIo extends LitElement {
       <main>
         <section class="short-description">
           <p>
-            API Console automates API documentation process by generating an UI
-            from <b>RAML</b> and <b>OAS</b> files.
+            API Console automates API documentation process <br />
+            by generating an UI from <b>RAML</b> and <b>OAS</b> files.
           </p>
         </section>
 
         <section class="ss-carousel">
           <div class="ss-img">
             <figure>
-              <img src="/resources/api-images/ss-method.png"/>
+              <img
+                src="/resources/api-images/ss-method.png"
+                alt="Screenshot of API method documentation"
+              />
               <figcaption>API method documentation</figcaption>
             </figure>
           </div>
           <div class="ss-img">
             <figure>
-              <img src="/resources/api-images/ss-type-doc.png"/>
+              <img
+                src="/resources/api-images/ss-type-doc.png"
+                alt="Screenshot of type documentation"
+              />
               <figcaption>A type documentation</figcaption>
             </figure>
           </div>
           <div class="ss-img">
             <figure>
-              <img src="/resources/api-images/ss-examples.png"/>
+              <img
+                src="/resources/api-images/ss-examples.png"
+                alt="Screenshot of example documentation"
+              />
               <figcaption>Generated examples</figcaption>
             </figure>
           </div>
           <div class="ss-img">
             <figure>
-              <img src="/resources/api-images/ss-tryit.png"/>
+              <img
+                src="/resources/api-images/ss-tryit.png"
+                alt="Screenshot of the request editor"
+              />
               <figcaption>API testing with pre-populated values</figcaption>
             </figure>
           </div>
           <div class="ss-img">
             <figure>
-              <img src="/resources/api-images/ss-mobile.png"/>
+              <img src="/resources/api-images/ss-mobile.png" alt="Screenshot of mobile view" />
               <figcaption>Mobile view build-in into design</figcaption>
             </figure>
           </div>
         </section>
 
-        <section class="use-reasons">
+        <section class="use-reasons info-section">
           <div class="image">
-            <img src="/resources/api-images/ss-mobile2.png" width="403" height="386" class="shadow-image"/>
+            <img
+              src="/resources/api-images/ss-mobile2.png"
+              width="403"
+              height="386"
+              class="shadow-image"
+              alt="Screenshot of query parameters documentation"
+            />
           </div>
           <div class="desc">
             <h2>Document everything, easily</h2>
             <ul class="checked-list">
               <li>Support for <b>RAML</b> and <b>OAS</b> documents is built-in by default.</li>
-              <li>Automatic API documentation generation from <b>AMF</b> data model. Use AMF parser to process API files.</li>
-              <li>API Console can be embedded inside existing application as a web component or be used as a standalone application.</li>
+              <li>
+                Automatic API documentation generation from <b>AMF</b> data model. Use AMF parser to
+                process API files.
+              </li>
+              <li>
+                API Console can be embedded inside existing application as a web component or be
+                used as a standalone application.
+              </li>
             </ul>
           </div>
         </section>
@@ -85,20 +109,41 @@ export class ApiConsoleIo extends LitElement {
             <b>API Designer</b>, <b>Exchange</b>, and <b>Studio</b>.
           </p>
           <p class="medium-desc">
-            These products are used by enterprise customers. We work hard
-            to ensure our API documentation is a world-class tool.
+            These products are used by enterprise customers. We work hard to ensure our API
+            documentation is a world-class tool.
           </p>
 
           <div class="dual-screens">
             <figure>
-              <img src="/resources/api-images/ss-designer.png"/>
+              <img
+                src="/resources/api-images/ss-designer.png"
+                alt="Screenshot of MuleSoft API designer"
+              />
               <figcaption>API Designer</figcaption>
             </figure>
             <figure>
-              <img src="/resources/api-images/ss-exchange.png"/>
+              <img
+                src="/resources/api-images/ss-exchange.png"
+                alt="Screenshot of Anypoint Exchange"
+              />
               <figcaption>Anypoint Exchange</figcaption>
             </figure>
           </div>
+        </section>
+
+        <section class="work-flow info-section">
+          <h2>Works great with MuleSoft API tooling</h2>
+          <img
+            src="/resources/general-data-flow.png"
+            width="682"
+            height="244"
+            alt="API tooling use flow"
+          />
+          <p>
+            API Console uses data model generated by the
+            <a href="https://github.com/aml-org/amf" target="_blank" rel="noreferrer">AMF parser</a>
+            from RAML or OAS project.
+          </p>
         </section>
       </main>
     `;
--- components/api-console-io/src/Styles.js
+++ components/api-console-io/src/Styles.js
@@ -1,214 +1,276 @@
 import { css } from 'lit-element';
 
 export default css`
-:host {
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-}
+  :host {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
 
-header {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: calc(100vh / 2);
-  position: absolute;
-  left: 0;
-  right: 0;
-  background-color: #2196F3;
-  background: linear-gradient(167deg, rgba(33,150,243,1) 46%, rgba(3,169,244,1) 100%);
-  box-shadow: 0 0 8px 0 rgba(0,0,0,.08),
-              0 0 15px 0 rgba(0,0,0,.02),
-              0 0 20px 4px rgba(0,0,0,.06);
-}
+    --header-height: 50vh;
+  }
 
-h1 {
-  font-size: 7rem;
-  font-weight: 300;
-  letter-spacing: -.044em;
-  color: #fff;
-  box-sizing: border-box;
-  margin: 0;
-}
+  header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: var(--header-height);
+    position: absolute;
+    left: 0;
+    right: 0;
+    background-color: #2196f3;
+    background: linear-gradient(167deg, rgba(33, 150, 243, 1) 46%, rgba(3, 169, 244, 1) 100%);
+    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.08), 0 0 15px 0 rgba(0, 0, 0, 0.02),
+      0 0 20px 4px rgba(0, 0, 0, 0.06);
+  }
 
-.subtitle {
-  color: #fff;
-  font-size: 3rem;
-  font-weight: 200;
-  margin-top: 80px;
-}
-
-main {
-  margin-top: calc(100vh / 2);
-  width: 100%;
-  max-width: 1680px;
-}
-
-.short-description {
-  text-align: center;
-  margin: 5rem 0;
-  font-size: 2rem;
-  color: #212121;
-  font-weight: 400;
-}
-
-.ss-carousel {
-  white-space: nowrap;
-  height: auto;
-  width: 100%;
-  overflow-x: scroll;
-  scroll-snap-type: x proximity;
-  background-color: white;
-  margin: 60px 0;
-}
-
-.ss-img {
-  scroll-snap-align: start;
-  display: inline-block;
-  vertical-align: middle;
-  margin: 8px 16px;
-}
-
-.ss-img figure {
-  margin: 0;
-  display: flex;
-  flex-direction: column;
-}
-
-.ss-img figcaption {
-  background-color: #0ba3f3;
-  color: black;
-  padding: 4px 8px;
-}
-
-.ss-img img {
-  height: 600px;
-}
-
-.button-anchor {
-  text-decoration: none;
-}
-
-.main-action-buttons {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin: 40px 0 0 0;
-}
-
-.main-action-buttons anypoint-button {
-  margin: 8px 0;
-  color: white;
-}
-
-.main-action-buttons a:focus {
-  outline: none;
-}
-
-.main-action-buttons a:focus anypoint-button {
-  outline: #fff auto 1px;
-}
-
-.main-action-buttons anypoint-button[emphasis="medium"] {
-  background-color: #1565C0;
-  border-color: transparent;
-}
-
-h2.section-title {
-  font-size: 3rem;
-  text-align: center;
-}
-
-.medium-desc {
-  font-size: 1.8rem;
-  text-align: center;
-}
-
-.platform-cases {
-  background-color: #F5F5F5;
-  padding: 48px 0;
-  margin: 40px 0;
-}
-
-.dual-screens {
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  width: 100%;
-  margin: 40px 0;
-}
-
-.dual-screens figure {
-  flex: 1;
-}
-
-.dual-screens img {
-  width: 100%;
-}
-
-.use-reasons {
-  margin: 60px 0;
-  display: flex;
-  flex-direction: row;
-}
-
-.use-reasons .image,
-.use-reasons .desc {
-  flex: 1;
-
-}
-
-.use-reasons .image {
-  text-align: center;
-}
-
-.shadow-image {
-  box-shadow: 0px 0px 20px 2px #00000038;
-}
-
-.checked-list {
-  list-style: none;
-  margin: 0;
-  padding: 0;
-  font-size: 1rem;
-  max-width: 500px;
-}
-
-.checked-list > li {
-  padding-left: calc(4px + 1.75em);
-  padding-right: 4px;
-  margin-bottom: 0.75em;
-}
-
-.checked-list > li::before {
-  content: '✓';
-  color: #3e4348;
-  overflow: hidden;
-  display: inline-block;
-  position: absolute;
-  width: 1.75em;
-  margin-left: -1.75em;
-  text-align: left;
-}
-
-@media (max-width: 1200px) {
   h1 {
-    font-size: 54px;
-    text-align: center;
-    margin: 0 24px;
-    line-height: 64px;
+    font-size: 7rem;
+    font-weight: 300;
+    letter-spacing: -0.044em;
+    color: #fff;
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  h2 {
+    font-size: 1.7em;
+    font-weight: 800;
+    line-height: 1.6em;
   }
 
   .subtitle {
-    font-size: 28px;
+    color: #fff;
+    font-size: 3rem;
+    font-weight: 200;
+    margin-top: 80px;
   }
 
   main {
-    max-width: 100%;
-    margin: 50vh 24px auto;
+    margin-top: var(--header-height);
+    width: 100%;
+    max-width: 1680px;
   }
-}
+
+  .short-description {
+    text-align: center;
+    font-size: 1.64rem;
+    line-height: 2.4rem;
+    color: #212121;
+    font-weight: 400;
+    max-width: 900px;
+    margin: 6rem auto;
+  }
+
+  .ss-carousel {
+    white-space: nowrap;
+    height: auto;
+    width: 100%;
+    overflow-x: scroll;
+    scroll-snap-type: x proximity;
+    background-color: white;
+    margin: 60px 0;
+  }
+
+  .ss-img {
+    scroll-snap-align: start;
+    display: inline-block;
+    vertical-align: middle;
+    margin: 8px 16px;
+  }
+
+  .ss-img figure {
+    margin: 0;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .ss-img figcaption {
+    background-color: #0ba3f3;
+    color: black;
+    padding: 4px 8px;
+  }
+
+  .ss-img img {
+    height: 600px;
+  }
+
+  .button-anchor {
+    text-decoration: none;
+  }
+
+  .main-action-buttons {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin: 40px 0 0 0;
+  }
+
+  .main-action-buttons anypoint-button {
+    margin: 8px 0;
+    color: white;
+  }
+
+  .main-action-buttons a:focus {
+    outline: none;
+  }
+
+  .main-action-buttons a:focus anypoint-button {
+    outline: #fff auto 1px;
+  }
+
+  .main-action-buttons anypoint-button[emphasis='medium'] {
+    background-color: #1565c0;
+    border-color: transparent;
+  }
+
+  h2.section-title {
+    font-size: 3rem;
+    text-align: center;
+  }
+
+  .medium-desc {
+    font-size: 1.6rem;
+    line-height: 2.2rem;
+    text-align: center;
+  }
+
+  .platform-cases {
+    background-color: #f5f5f5;
+    padding: 48px 0;
+    margin: 40px 0;
+  }
+
+  .dual-screens {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 100%;
+    margin: 40px 0;
+  }
+
+  .dual-screens figure {
+    flex: 1;
+  }
+
+  .dual-screens img {
+    width: 100%;
+  }
+
+  .info-section {
+    margin: 8rem 0;
+    display: flex;
+  }
+
+  .use-reasons {
+    flex-direction: row;
+  }
+
+  .use-reasons .image,
+  .use-reasons .desc {
+    flex: 1;
+  }
+
+  .use-reasons .image {
+    text-align: center;
+  }
+
+  .shadow-image {
+    box-shadow: 0px 0px 20px 2px #00000038;
+  }
+
+  .checked-list {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    font-size: 1rem;
+    max-width: 500px;
+  }
+
+  .checked-list > li {
+    padding-left: calc(4px + 1.75em);
+    padding-right: 4px;
+    margin-bottom: 0.75em;
+  }
+
+  .checked-list > li::before {
+    content: '✓';
+    color: #3e4348;
+    overflow: hidden;
+    display: inline-block;
+    position: absolute;
+    width: 1.75em;
+    margin-left: -1.75em;
+    text-align: left;
+  }
+
+  .work-flow {
+    flex-direction: column;
+    align-items: center;
+  }
+
+  @media (max-height: 980px) {
+    :host {
+      --header-height: 70vh;
+    }
+  }
+
+  @media (min-height: 980px and max-height: 1100px) {
+    :host {
+      --header-height: 60vh;
+    }
+  }
+
+  @media (max-width: 1200px) {
+    h1 {
+      font-size: 54px;
+      text-align: center;
+      margin: 0 24px;
+      line-height: 64px;
+    }
+
+    .subtitle {
+      font-size: 28px;
+    }
+
+    main {
+      max-width: 100%;
+      margin: var(--header-height) 24px auto;
+    }
+  }
+
+  @media (max-width: 1024px) {
+    .dual-screens,
+    .use-reasons {
+      flex-direction: column;
+    }
+
+    .ss-img img {
+      width: initial;
+      height: 360px;
+      max-width: initial;
+    }
+
+    img {
+      margin: 0 12px;
+      max-width: calc(100% - 24px);
+      height: auto;
+    }
+
+    figure {
+      margin: 0;
+    }
+
+    .info-section {
+      margin-left: 24px;
+      margin-right: 24px;
+    }
+
+    .platform-cases {
+      padding-left: 24px;
+      padding-right: 24px;
+    }
+  }
 `;
