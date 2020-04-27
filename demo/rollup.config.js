bc6e4f6c5b3abc6fee0a931dead08f3a47491288
Pawel <jarrodek@gmail.com>
2020-04-27 14:51:38 -0700

    chore: upgrading rolup configuration

--- demo/rollup.config.js
+++ demo/rollup.config.js
@@ -1,47 +1,85 @@
-import { createDefaultConfig } from '@open-wc/building-rollup';
+import { createSpaConfig } from '@open-wc/building-rollup';
 import path from 'path';
+import merge from 'deepmerge';
 import postcss from 'rollup-plugin-postcss';
 import cpy from 'rollup-plugin-cpy';
 
-const config = createDefaultConfig({
-  input: path.join(__dirname, 'index.html'),
-  indexHTMLPlugin: {
-    minify: {
-      minifyJS: true,
-      removeComments: true,
-    },
-  },
+const baseConfig = createSpaConfig({
+  outputDir: 'demo-dist',
+  developmentMode: process.env.ROLLUP_WATCH === 'true',
+  injectServiceWorker: true,
 });
 
-config.output.dir = 'demo-dist';
-config.context = 'window';
 
-// console.log(config);
+baseConfig.context = 'window';
 
-export default [
-  {
-    ...config,
-    plugins: [
-      ...config.plugins,
-      postcss(),
-      cpy({
-        files: [
-          path.join(__dirname, 'vendor.js'),
-          // path.join(__dirname, 'models'),
-          path.join(__dirname, '*.css'),
-        ],
-        dest: 'demo-dist',
-        options: {
-          parents: false,
-        },
-      }),
-      cpy({
-        files: [path.join(__dirname, 'models', '*.json')],
-        dest: path.join('demo-dist', 'models'),
-        options: {
-          parents: false,
-        },
-      }),
-    ],
+export default merge(baseConfig, {
+  input: path.join(__dirname, 'index.html'),
+  output: {
+    sourcemap: false,
   },
-];
+  plugins: [
+    postcss(),
+    cpy({
+      files: [
+        path.join(__dirname, 'vendor.js'),
+        // path.join(__dirname, 'models'),
+        path.join(__dirname, '*.css'),
+      ],
+      dest: 'demo-dist',
+      options: {
+        parents: false,
+      },
+    }),
+    cpy({
+      files: [path.join(__dirname, 'models', '*.json')],
+      dest: path.join('demo-dist', 'models'),
+      options: {
+        parents: false,
+      },
+    }),
+  ],
+});
+
+// const config = createDefaultConfig({
+//   input: path.join(__dirname, 'index.html'),
+//   indexHTMLPlugin: {
+//     minify: {
+//       minifyJS: true,
+//       removeComments: true,
+//     },
+//   },
+// });
+//
+// config.output.dir = 'demo-dist';
+// config.context = 'window';
+//
+// // console.log(config);
+//
+// export default [
+//   {
+//     ...config,
+//     plugins: [
+//       ...config.plugins,
+//       postcss(),
+//       cpy({
+//         files: [
+//           path.join(__dirname, 'vendor.js'),
+//           // path.join(__dirname, 'models'),
+//           path.join(__dirname, '*.css'),
+//         ],
+//         dest: 'demo-dist',
+//         options: {
+//           parents: false,
+//         },
+//       }),
+//       cpy({
+//         files: [path.join(__dirname, 'models', '*.json')],
+//         dest: path.join('demo-dist', 'models'),
+//         options: {
+//           parents: false,
+//         },
+//       }),
+//     ],
+//   },
+// ];
--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,22 @@
-import { createDefaultConfig } from '@open-wc/building-rollup';
+import { createSpaConfig } from '@open-wc/building-rollup';
 import cpy from 'rollup-plugin-cpy';
+import merge from 'deepmerge';
 
-const config = createDefaultConfig({ input: './index.html' });
-config.output.dir = 'www-dist';
-export default {
-  ...config,
+const baseConfig = createSpaConfig({
+  outputDir: 'www-dist',
+  developmentMode: process.env.ROLLUP_WATCH === 'true',
+  injectServiceWorker: true,
+});
+
+
+export default merge(baseConfig, {
+  // if you use createSpaConfig, you can use your index.html as entrypoint,
+  // any <script type="module"> inside will be bundled by rollup
+  input: './index.html',
   output: {
-    ...config.output,
-    // sourcemap: false,
+    sourcemap: false,
   },
   plugins: [
-    ...config.plugins,
     cpy({
       files: [
         './resources/**/*.*',
@@ -24,4 +30,28 @@ export default {
       },
     }),
   ],
-};
+});
+//
+// config.output.dir = 'www-dist';
+// export default {
+//   ...config,
+//   output: {
+//     ...config.output,
+//     // sourcemap: false,
+//   },
+//   plugins: [
+//     ...config.plugins,
+//     cpy({
+//       files: [
+//         './resources/**/*.*',
+//         './index.css',
+//         './robots.txt',
+//         './humans.txt',
+//       ],
+//       dest: 'www-dist',
+//       options: {
+//         parents: true,
+//       },
+//     }),
+//   ],
+// };
